99573327257a1291747d5e2cd03dbd547afafef6
Seth Vargo <sethvargo@gmail.com>
2011-07-15 16:47:02 -0400

    fixing layout with fancybox

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,9 +1,13 @@
 class Admin::ApplicationController < ApplicationController
   before_filter :require_admin
-  layout 'administrator'
+  layout :process_layout
   
   private
   def require_admin
     redirect_to root_path, :alert => 'You must be an administrator to access that resource!' unless logged_in? && current_user.is_a?(Administrator)
+  end
+
+  def process_layout
+    request.xhr? ? nil : 'administrator'
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include CustomAuth
-  layout :process_layout
 
   before_filter :require_login
-  private
-  def process_layout
-    request.xhr? ? nil : 'application'
-  end
 end
